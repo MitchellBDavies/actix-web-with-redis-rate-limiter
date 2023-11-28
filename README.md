ffe6da5a0f1528db52cb274dfb85d105b7e153c1
Mitchell Davies <mitchellbdavies@gmail.com>
2023-11-27 21:51:26 -0600

    Set up successful Redis connections from the webserver.

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # actix-web-with-redis-rate-limiter
 A small repo implementing a Rust rate limiter on an Actix Web Server.
 
+Note that this system does not yet work fully, and I'd recommend against using this in a prod system. I've had trouble finding easy documentation to use for this project at the moment, and at the time of writing this readME, actix-redis is listed as "maybe insecure" on their GitHub repo: https://github.com/actix/actix-extras.
 
 # How to run:
 The project can be ran with the following command:
 
 ```
-docker-compose up -d
+docker-compose up --build --force-recreate -d
 ```
--- actix_server/src/main.rs
+++ actix_server/src/main.rs
@@ -1,14 +1,47 @@
-use actix_web::{get, web, App, HttpServer, Responder};
-use serde::Deserialize;
+use actix_web::{get, App, web::Data, HttpServer, HttpRequest, Responder};
+use actix_redis::{Command, Error, RedisActor, RespValue, resp_array};
+use actix::prelude::Addr;
 
 #[get("/")]
 async fn index() -> impl Responder {
     "Hello, World!"
 }
 
+#[get("/ip")]
+async fn ip(req: HttpRequest, redis_actor: Data<Addr<RedisActor>>) -> impl Responder {
+    let set = redis_actor.send(Command(resp_array!["SET", "key", "value"])).await;
+
+    let get = redis_actor.send(Command(resp_array!["GET", "key"])).await;
+    match get {
+        Ok(Ok(respValue)) => {
+            println!("{:?}", respValue);
+        },
+        _ => panic!("Should not happen {:?}", get),
+    }
+
+    if let Some(val) = req.peer_addr() {
+        format!("Address {:?}", val.ip())
+    } else {
+        "No IP Address found".to_string()
+    }
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index))
+
+    std::env::set_var("RUST_LOG", "debug");
+    env_logger::init();
+
+    println!("Actix Web Server is starting.");
+
+    let redis_actor = RedisActor::start("redis:6379");
+
+    HttpServer::new(
+        move || App::new()
+            .service(index)
+            .service(ip)
+            .app_data(Data::new(redis_actor.clone()))
+        )
         .bind(("0.0.0.0", 8080))?
         .run()
         .await
