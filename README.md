7cc98bf8234c46dab6663fdb62efddbc2443e88e
Mitchell Davies <mitchellbdavies@gmail.com>
2023-11-29 22:53:47 -0600

    Rate limiting now works!

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 # actix-web-with-redis-rate-limiter
-A small repo implementing a Rust rate limiter on an Actix Web Server.
+A small repo implementing a Rust rate limiter on an Actix Web Server utilizing Redis.
 
-Note that this system does not yet work fully, and I'd recommend against using this in a prod system. I've had trouble finding easy documentation to use for this project at the moment, and at the time of writing this readME, actix-redis is listed as "maybe insecure" on their GitHub repo: https://github.com/actix/actix-extras.
+The Rate Limiter will begin rate limiting requests after a user makes more than 10 requests per 30 seconds. 
+
+Note that this system does not yet work fully, and I would highly recommend against using this in a prod system. I've had trouble finding easy documentation to use for this project at the moment, and at the time of writing this readME, actix-redis is listed as "maybe insecure" on their GitHub repo: https://github.com/actix/actix-extras.
 
 # How to run:
-The project can be ran with the following command:
+This code and it's libraries have not been fully scanned for vulnerabilities or bugs, so please **run at your own risk.**
+
+**I am currently experiencing an issue where repeatedly running these containers will cause my WSL to hog a significant amount of memory that is not freed when containers are exited or docker enginer is powered off. I specifically must force the WSL to shutdown to release this memory. I am unsure if this is an issue with my project specifically.**
+
+If you are interested in running the code, the project can be ran with the following command:
 
 ```
 docker-compose up --build --force-recreate -d
 ```
+
+Then, begin making requests to the server. I find that I can reach the server at 127.0.0.1:8080 when running these as container on docker on my local machine. '/' is not rate limited, but '/ratelimit' is. If you make more than 10 requests in a 30 second window, you should receive an HTTP 429 status code. 
--- actix_server/src/main.rs
+++ actix_server/src/main.rs
@@ -1,28 +1,77 @@
-use actix_web::{get, App, web::Data, HttpServer, HttpRequest, Responder};
-use actix_redis::{Command, Error, RedisActor, RespValue, resp_array};
+use actix_web::{get, App, web::Data, HttpServer, HttpRequest, HttpResponse, Responder, http::StatusCode};
+use actix_redis::{Command, RedisActor, resp_array};
 use actix::prelude::Addr;
+use std::net::IpAddr;
+
+use std::time::SystemTime;
+
+enum RateLimitResult {
+    RateLimit,
+    InternalError,
+    NotLimited
+}
 
 #[get("/")]
 async fn index() -> impl Responder {
     "Hello, World!"
 }
 
-#[get("/ip")]
-async fn ip(req: HttpRequest, redis_actor: Data<Addr<RedisActor>>) -> impl Responder {
-    let set = redis_actor.send(Command(resp_array!["SET", "key", "value"])).await;
+fn redis_schema_ip_sount_set(ip_address: IpAddr) -> String {
+    "ip_count_set:".to_owned() + &ip_address.to_string()
+}
 
-    let get = redis_actor.send(Command(resp_array!["GET", "key"])).await;
-    match get {
-        Ok(Ok(respValue)) => {
-            println!("{:?}", respValue);
+fn redis_schema_max_requests() -> i64 {
+    10
+}
+
+fn redis_schema_rate_limit_window() -> i64 {
+    30000
+}
+
+async fn check_ip_rate_limit(ip_address: IpAddr, redis_actor: Data<Addr<RedisActor>>) -> RateLimitResult {
+    //let set = redis_actor.send(Command(resp_array!["SET", "key", "value"])).await;
+
+    let ip_count_key = redis_schema_ip_sount_set(ip_address);
+    let current_time = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_millis() as i64;
+    let window_start = current_time - redis_schema_rate_limit_window();
+    let ip_count_in_window = redis_actor.send(Command(resp_array!["ZCOUNT", &ip_count_key, window_start.to_string(), current_time.to_string()])).await;
+
+    println!("{:?}", ip_count_in_window);
+
+    match ip_count_in_window {
+        Ok(Ok(redis_async::resp::RespValue::Integer(requests_in_window))) => {
+            if requests_in_window < redis_schema_max_requests() {
+                let _ = redis_actor.send(Command(resp_array!["ZADD", &ip_count_key, current_time.to_string(), current_time.to_string()])).await;
+                let _ = redis_actor.send(Command(resp_array!["ZREMRANGEBYSCORE", &ip_count_key, "-inf", window_start.to_string()])).await;
+                RateLimitResult::NotLimited
+            } else {
+                RateLimitResult::RateLimit
+            }            
         },
-        _ => panic!("Should not happen {:?}", get),
+        _ => RateLimitResult::InternalError
     }
+}
+
+#[get("/ratelimit")]
+async fn ip(req: HttpRequest, redis_actor: Data<Addr<RedisActor>>) -> impl Responder {
 
     if let Some(val) = req.peer_addr() {
-        format!("Address {:?}", val.ip())
+        let ip_address = val.ip();
+        match check_ip_rate_limit(ip_address, redis_actor).await {
+            RateLimitResult::NotLimited  => {
+                HttpResponse::Ok()
+                    .body(format!("Address {:?}", ip_address))
+            },
+            RateLimitResult::RateLimit => {
+                HttpResponse::new(StatusCode::TOO_MANY_REQUESTS)
+            },
+            RateLimitResult::InternalError => {
+                HttpResponse::new(StatusCode::INTERNAL_SERVER_ERROR)
+            }
+        }
+        
     } else {
-        "No IP Address found".to_string()
+        HttpResponse::new(StatusCode::INTERNAL_SERVER_ERROR)
     }
 }
 
